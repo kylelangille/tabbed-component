013f38a519ba4ad5bbe5b416f0cae5a329bf1b20
k l <103784140+kyleisconfused@users.noreply.github.com>
2022-07-12 13:12:19 -0230

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,13 +1,16 @@
 "use strict";
 
+//Grabbing elements
 const tabs = document.querySelectorAll(".tab");
 const tabContainer = document.querySelector(".tab-container");
 const tabContent = document.querySelectorAll(".content");
 
+//Main functionality
 tabContainer.addEventListener("click", function (e) {
   const clicked = e.target.closest(".tab");
   console.log(clicked);
 
+  //Guard clause
   if (!clicked) return;
 
   // Hide content
